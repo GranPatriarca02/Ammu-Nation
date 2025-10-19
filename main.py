b772b6d195920e9bf53a0e8bb7ead254bca2309a
Tony <a.mitev098@gmail.com>
2025-10-19 19:37:51 +0200

    Update .env
    Update .gitignore
    
    Updated main to be able to run the program

---
+++ main.py
@@ -0,0 +1,28 @@
+# ARCHIVO PRINCIPAL.
+
+# Accedemos a la clase tablas y usamos el objeto cursor para lanzar las consultas.
+from tablas import cursor
+
+# TEMPORAL:
+## Mostramos el contenido de las TABLAS de forma temporal, se debe crear un metodo.
+def main():
+    try:
+        # Almacenamos las tablas que vamos a recorrer: 
+        tablas = ["CATEGORIAS", "TIPO_ARMA", "CALIBRES", "FABRICANTES", "PRODUCTOS"]
+        # Recorremos las tablas
+        for recorrerTabla in tablas:
+            print(f"\n CONTENIDO: {recorrerTabla}")
+            # Consulta que recorre cada una de las tablas del array TABLAS.
+            cursor.execute(f"SELECT * FROM {recorrerTabla}")
+            filas = cursor.fetchall()
+            # Recorremos las filas de las tablas
+            if filas:
+                for recorrerFila in filas:
+                    print(recorrerFila)
+            else:
+                print("La tabla se encuentra vacia")
+    except Exception as e:
+        print("ERROR: ", e)
+            
+main()
+
