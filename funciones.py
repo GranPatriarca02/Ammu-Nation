9d25b0b8fd618b59336ebf5fc0b0b62ec6b4b174
Tony <a.mitev098@gmail.com>
2025-10-25 16:55:57 +0200

    Update funciones.py: Method validarInt.

--- funciones.py
+++ funciones.py
@@ -1,21 +1,34 @@
 # Importamos los elementos cursor y actualizarCommit del archivo db.py
 from db import cursor, actualizarCommit
 
+
+
+
+# Creamos un bucle para validar los numeros que introduzca el usuario.
+def validarInt(mensaje):
+    while True:
+        valor = input(mensaje).strip()
+        if valor.isdigit():
+            return int(valor)
+        else:
+            print("ERROR: Debes introducir un numero.")
+
 def crearProducto():
     try:
         codigo_serie = input("Codigo de serie: ").strip()
         nombre = input("Nombre del producto:").strip()
-        id_calibre = int(input("Selecciona el calibre: "))
-        id_tipo_arma = int(input("Selecciona el tipo de arma: "))
-        id_categoria = int(input("Selecciona el tipo de categoria: "))
-        id_fabricante = int(input("Selecciona el fabricante: "))
-        stock = int(input("Cantidad de stock: "))
-        precio = int(input("Introduce el precio del producto: "))
+        id_calibre = validarInt("Selecciona el calibre: ")
+        id_categoria = validarInt("Selecciona la categoria: ")
+        id_tipo_arma = validarInt("Selecciona el tipo de arma: ")
+        id_categoria =  validarInt("Selecciona el tipo de categoria: ")
+        id_fabricante =  validarInt("Selecciona el fabricante: ")
+        stock =  validarInt("Cantidad de stock:  ")
+        precio =  validarInt("Introduce el precio del producto: ")
         descripcion = input("Introduce la descripcion del producto: ").strip()
 
         # Creamos la consulta para insertar los datos:
         cursor.execute('''
-            INSERT IN TO PRODUCTOS (
+            INSERT INTO PRODUCTOS (
                 CODIGO_SERIE, NOMBRE, ID_CALIBRE, ID_TIPO_ARMA, ID_CATEGORIA, ID_FABRICANTE, STOCK, PRECIO, DESCRIPCION)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             ''', (codigo_serie, nombre, id_calibre, id_tipo_arma, id_categoria, id_fabricante, stock, precio, descripcion))
